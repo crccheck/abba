7d74674531b3662e54256a1aca5254c2b41cafc1
crccheck <c@crccheck.com>
2015-09-12 21:06:26 -0500

    add basic hello world

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('moo');
+})
+
+var server = app.listen(8000, () => {
+  console.log(server.address());
+})
