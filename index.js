2ae386de2455f4035a94af381c6b88005c3b34fe
crccheck <c@crccheck.com>
2015-09-12 21:50:27 -0500

    get normal

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
+// To see what generators are available, seE:
+// https://github.com/robbrit/randgen/blob/master/lib/randgen.js
+const randgen = require('randgen');
 const express = require('express');
+
 const app = express();
 
 
 app.get('/', (req, res) => {
+  // TODO send a list of what resources are available
   res.send('moo');
 });
 
-var server = app.listen(8000, () => {
-  console.log(server.address());
+app.get('/gaussian/:mean?/:stdev?', (req, res) => {
+  const mean = req.params.mean || 100;
+  const stdev = req.params.stdev || 10;
+
+  const delay = Math.max(0, randgen.rnorm(parseFloat(mean), parseFloat(stdev)));
+  // TODO if accepts is json, return json
+  res.send(`Gaussian mean: ${mean} standard deviation: ${stdev} delay: ${delay}`);
+});
+
+
+const server = app.listen(8000, () => {
+  console.log(`Server is online; listening on port ${server.address().port}`);
 });
